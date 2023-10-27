3459a9ca19d05ab61bd8ebbb0ce5335042ae5528
Christinne <vanessa0christinne@gmail.com>
2023-10-27 22:11:28 +0200

    Update main.py
    - Fix import FileManager

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from bank_system.client import Client, FileManager
+from bank_system.client import Client
+from bank_system.file_manager import FileManager
 from bank_system.history import History
 from types_bank_account import *
 
