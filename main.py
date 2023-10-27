64eb185daa242f64a14a9cba28fea7b8ac9a36a0
Christinne <vanessa0christinne@gmail.com>
2023-10-27 18:25:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ def initialize_checking_account(client_data, account_data):
     account = CheckingAccount(client.name, *account_data)
     history = History(account.number)
 
-    # name | number | balance | limit
+    with open("checking-accounts.txt", "r") as file:
+        existing_accounts = file.readlines()
+        for line in existing_accounts:
+            if all(item in line for item in [client_data[0], account_data[1], str(account_data[2]), str(account_data[3])]):
+                return client, account, history
+
     with open("checking-accounts.txt", "a") as file:
-        file.write(f"{client_data[2]} | {account_data[1]} | {account_data[2]} | {account_data[3]}\n")
+        file.write(f"{client_data[0]} | {account_data[1]} | {account_data[2]} | {account_data[3]}\n")
 
     return client, account, history
 
@@ -40,42 +45,19 @@ def initialize_investment_account(client_data, account_data):
 
 
 def load_existing_accounts():
-    clients = []
     accounts = []
-    with open("accounts.txt", "r") as file:
+    with open("checking-accounts.txt", "r") as file:
         for line in file:
             cpf, number, balance, limit = line.strip().split("|")
             client = Client("", "", cpf)
             account = CheckingAccount(client.name, number, float(balance), float(limit))
-            clients.append(client)
             accounts.append(account)
-    return clients, accounts
+    return accounts
 
 
 def main():
-    client_1_data = ("Nicole", "Watterson", "11111111111-1")
-    account_1_data = ("123", 2500.0, 5000.0)
-
-    client_2_data = ("Ricardo", "Watterson", "22222222222-2")
-    account_2_data = ("456", 500.0, 2000.0)
-
-    client_3_data = ("Gumball", "Watterson", "33333333333-3")
-    account_3_data = ("789", 5.0, 500.0)
-
-    client_1, checking_account_1, history_1 = initialize_checking_account(client_1_data, account_1_data)
-    client_2, saving_account_1, history_2 = initialize_saving_account(client_2_data, account_2_data)
-    client_3, investment_account_1, history_3 = initialize_investment_account(client_3_data, account_3_data)
-
     deposit_value = 10
-    # withdraw_value = 10
     # transfer_value = 500
-
-    '''
-    try:
-        checking_account_1.withdraw(withdraw_value)
-    except ValueError as e:
-        print(e)
-    '''
 
     '''
     try:
@@ -91,14 +73,7 @@ def main():
         print(e)
     '''
 
-    checking_account_1.update()
-    print(checking_account_1)
-
-    saving_account_1.update()
-    print(saving_account_1)
-
-    investment_account_1.update()
-    print(investment_account_1)
+    print(load_existing_accounts())
 
 
 if __name__ == '__main__':
