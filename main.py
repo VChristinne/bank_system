177eb812080bd82cadbd17710aa44d4540850980
Christinne <vanessa0christinne@gmail.com>
2023-10-29 20:00:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import ttkbootstrap as ttkb
 
 
 def main():
-    root = ttkb.Window(title="Christinne S.A",
+    root = ttkb.Window(title="Christinne S.A.",
                        themename="superhero",
                        size=[800, 500],
                        position=[450, 180])
