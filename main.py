eaa5148ae1590ea0f6e32d5d48dc9a06c33416e6
Christinne <vanessa0christinne@gmail.com>
2023-11-02 16:13:23 +0100

    Update menu screen

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import customtkinter as ctk
 from types_bank_account import *
 
 ctk.set_appearance_mode("Dark")
-ctk.set_default_color_theme("theme/dark-blue.json")
+ctk.set_default_color_theme("theme/indigo.json")
 
 app = ctk.CTk()
 app.geometry("800x600")
@@ -30,12 +30,12 @@ l1 = ctk.CTkLabel(master=app, image=bg)
 l1.pack()
 
 # custom frame
-frame = ctk.CTkFrame(master=l1, width=320, height=360, corner_radius=15, bg_color="transparent")
+frame = ctk.CTkFrame(master=l1, width=320, height=360)
 frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 # log in text
-l2 = ctk.CTkLabel(master=frame, text="Log into your Account", font=('SF Pro', 20))
-l2.place(x=50, y=45)
+l2 = ctk.CTkLabel(master=frame, text="Log in to your account", font=('SF Pro', 20))
+l2.place(x=60, y=45)
 
 # usarname
 entry_username = ctk.CTkEntry(master=frame, width=220, placeholder_text='Username')
@@ -44,10 +44,6 @@ entry_username.place(x=50, y=110)
 # password
 entry_password = ctk.CTkEntry(master=frame, width=220, placeholder_text='Password', show="*")
 entry_password.place(x=50, y=165)
-
-# forget password
-l3 = ctk.CTkLabel(master=frame, text="Forget password?", font=('SF Pro', 12))
-l3.place(x=155, y=195)
 
 # button login
 btn_login = ctk.CTkButton(master=frame, width=220, text="Login", command=button_function, corner_radius=6)
