9b51221c0f3ac0bd54b3a70e0cbb13b3c2358406
Christinne <vanessa0christinne@gmail.com>
2023-11-02 17:12:21 +0100

    Verify username and password

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter
+from tkinter import messagebox
 from PIL import ImageTk, Image
 import customtkinter as ctk
+from file_manager import FileManager
 from types_bank_account import *
 
 ctk.set_appearance_mode("Dark")
@@ -11,17 +13,41 @@ app.geometry("800x600")
 app.title("Login")
 app.wm_attributes('-fullscreen', True)
 
+
 # account_1 = CheckingAccount.load_account("12345")
 
+def load_data():
+    try:
+        data = FileManager.load_data('files_json/accounts_list.json')
+        messagebox.showinfo("Success", "Data loaded successfully")
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
 
-def button_function():
-    app.destroy()
-    window = ctk.CTk()
-    window.geometry("1280x720")
-    window.title("Welcome")
-    label1 = ctk.CTkLabel(master=window, text="Home Page", font=('SF Pro', 60))
-    label1.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-    window.mainloop()
+
+def save_data():
+    try:
+        data = {}
+        FileManager.save_data('files_json/accounts_list.json', data)
+        messagebox.showinfo("Success", "Data saved successfully")
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+
+def button_function(username, password):
+    data = FileManager.load_data('files_json/accounts_list.json')
+
+    for client in data:
+        if client['holder'] == username and client['password'] == password:
+            app.destroy()
+            window = ctk.CTk()
+            window.geometry("1280x720")
+            window.title("Welcome")
+            label1 = ctk.CTkLabel(master=window, text="Home Page", font=('SF Pro', 60))
+            label1.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+            window.mainloop()
+            return
+
+    messagebox.showerror("Error", "Invalid username or password")
 
 
 def menu_frame():
@@ -47,7 +73,9 @@ def menu_frame():
     entry_password.place(x=50, y=165)
 
     # button login
-    btn_login = ctk.CTkButton(master=frame, width=220, text="Login", command=button_function, corner_radius=6)
+    btn_login = ctk.CTkButton(master=frame, width=220, text="Login",
+                              command=lambda: button_function(entry_username.get(), entry_password.get()),
+                              corner_radius=6)
     btn_login.place(x=50, y=240)
 
 
